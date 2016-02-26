ed4f2b6a0e69b697cfc6006808e94fa8f3ff0815
oswystan <oswystan@126.com>
2016-02-26 15:03:05 +0800

    make http query team list works

--- controller/filter.go
+++ controller/filter.go
@@ -14,10 +14,22 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/oswystan/fixer/datastore"
 	"github.com/oswystan/fixer/model"
 )
+
+func marshalResult(w http.ResponseWriter, v interface{}, code int) {
+	bs, err := json.MarshalIndent(v, "", "\t")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	bs = append(bs, '\n')
+	w.WriteHeader(code)
+	w.Write(bs)
+}
 
 func ServeFilterMemberList(w http.ResponseWriter, r *http.Request) {
 	log.Printf("serve member list %s", r.URL.Path)
@@ -29,7 +41,23 @@ func ServeFilterTeamList(w http.ResponseWriter, r *http.Request) {
 
 	l := &model.ResultTeamList{}
 	tl := datastore.NewStoreTeamList()
-	filter := &datastore.FilterTeamList{LeaderId: 1}
+	filter := &datastore.FilterTeamList{}
+
+	query := r.URL.Query()
+
+	for k, v := range query {
+		switch k {
+		case "creatorid":
+			filter.LeaderId, _ = strconv.Atoi(v[0])
+		case "creator":
+			filter.LeaderName = v[0]
+
+		case "memberid":
+			filter.MemberId, _ = strconv.Atoi(v[0])
+		case "member":
+			filter.MemberName = v[0]
+		}
+	}
 
 	l.Teams, err = tl.GetTeamList(filter)
 	if err != nil {
@@ -37,14 +65,7 @@ func ServeFilterTeamList(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	bs, err := json.MarshalIndent(l, "", "\t")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	bs = append(bs, '\n')
-	w.WriteHeader(http.StatusOK)
-	w.Write(bs)
+	marshalResult(w, l, http.StatusOK)
 }
 
 func ServeFilterBugList(w http.ResponseWriter, r *http.Request) {
