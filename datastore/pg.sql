c75ae9a6f1341bd2ed6ec605d2db399b83de7ca0
oswystan <oswystan@126.com>
2016-02-27 08:09:33 +0800

    add buglog table, index to bugs and buglog, test data to bugs,buglog.

--- datastore/pg.sql
+++ datastore/pg.sql
@@ -52,13 +52,14 @@ CREATE TABLE user_team (
 CREATE TABLE bugs (
     id              SERIAL NOT NULL,
     created_by      INT NOT NULL,
-    title           VARCHAR(128) not null,
-    priority        INT NOT NULL,
-    detail          VARCHAR(4096), 
-    attachments     VARCHAR(512), 
     current_handler INT NOT NULL,
+    priority        INT NOT NULL,
     status          INT, 
+    created_time    TIMESTAMP,
     last_update     TIMESTAMP,
+    title           VARCHAR(128) not null,
+    attachments     VARCHAR(512), 
+    detail          VARCHAR(4096), 
 
     PRIMARY KEY(id)
 );
@@ -67,20 +68,28 @@ CREATE TABLE buglog (
     bug_id          INT NOT NULL,
     who             INT NOT NULL,
     action_type     INT NOT NULL,
-    action          VARCHAR(512), 
-    action_time     TIMESTAMP
+    action_time     TIMESTAMP,
+    action          VARCHAR(4096) 
 );
 
 -- index of tables
-CREATE UNIQUE INDEX users_nicky ON users(nicky);
-CREATE UNIQUE INDEX team_name ON team(name);
-CREATE INDEX team_leader ON team(leader_id);
+CREATE UNIQUE INDEX idx_users_nicky ON users(nicky);
+CREATE UNIQUE INDEX idx_team_name ON team(name);
+CREATE INDEX idx_team_leader ON team(leader_id);
+CREATE INDEX idx_bugs_createdby ON bugs(created_by);
+CREATE INDEX idx_bugs_priority ON bugs(priority);
+CREATE INDEX idx_bugs_handler ON bugs(current_handler);
+CREATE INDEX idx_bugs_status ON bugs(status);
+CREATE INDEX idx_bugs_created_time ON bugs(created_time);
+CREATE INDEX idx_bugs_last_update ON bugs(last_update);
+CREATE INDEX idx_buglog_bugid ON buglog(bug_id);
 
 ALTER TABLE users OWNER TO pgtest;
 ALTER TABLE team OWNER TO pgtest;
 ALTER TABLE user_team OWNER TO pgtest;
 ALTER TABLE bugs OWNER TO pgtest;
 ALTER TABLE buglog OWNER TO pgtest;
+
 ALTER SEQUENCE users_id_seq OWNER TO pgtest;
 ALTER SEQUENCE team_id_seq OWNER TO pgtest;
 
--- datastore/test-data.sql
+++ datastore/test-data.sql
@@ -10,6 +10,8 @@
 
 \c fixer;
 
+BEGIN;
+
 -- Add new users
 insert into users(nicky, email, pwd, register_date, last_login_time, portrait) 
        values ('john', 'john@gmail.com', '123456', now(), now(), 'static/images/2.jpg');
@@ -35,9 +37,26 @@ insert into user_team values(2,1,now());
 insert into user_team values(2,2,now());
 insert into user_team values(2,3,now());
 
+-- Add bugs and log
+insert into bugs(created_by, current_handler, priority, status, created_time, last_update, title, attachments, detail) 
+    values(1, 2, 1, 1, now(), now(), 'system crashed', null, 
+    'the program coredumped, need to fixed AS QUICKLY AS POSSIBLE');
+insert into bugs(created_by, current_handler, priority, status, created_time, last_update, title, attachments, detail) 
+    values(2, 1, 1, 1, now(), now(), 'we can not login to the system', null, 
+    'login page can not be shown, so fix it.');
+insert into buglog(bug_id, who, action_type, action_time, action)
+    values(1, 1, 1, now(), 'assigned to user sherklock(2)');
+insert into buglog(bug_id, who, action_type, action_time, action)
+    values(2, 1, 1, now(), 'assigned to user john(1)');
+
 select id, nicky, email, pwd from users order by id;
 select id, name, leader_id, status, bug_table from team order by id;
 select * from user_team order by user_id, team_id;
+select id, created_by, current_handler, priority, status, title from bugs;
+select * from buglog;
+
+-- commit tx
+COMMIT;
 
 
 ---------------------------------------------------------------------------------
