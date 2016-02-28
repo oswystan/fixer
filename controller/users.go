41154c24a1261cb8c54cfec41ff695a669b0d81f
oswystan <oswystan@126.com>
2016-02-29 00:08:16 +0800

    add empty controller files for restful api.

---
+++ controller/users.go
@@ -0,0 +1,47 @@
+//===============================================================================
+//                      Copyright (C) 2016 wystan
+//
+//        filename: users.go
+//     description:
+//         created: 2016-02-28 19:05:49
+//          author: wystan
+//
+//===============================================================================
+
+package controller
+
+import "net/http"
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteUsers(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func PostUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func PutUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetUserTeamsJoined(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetUserTeamsCreated(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//==================================== END ======================================
